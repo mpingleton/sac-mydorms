98cfac992e42b6e7ac41b589488b84630ac23d67
Nicholas Phillips <nicholas.d.phillips@gmail.com>
2021-09-22 23:13:34 -0500

    Fix Heroku build

--- backend/src/app.js
+++ backend/src/app.js
@@ -65,7 +65,7 @@ app.use('/api/', routes);
 // Frontend (i.e. React)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
-  app.get('/', (req, res) => {
+  app.get('(/*)?', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
   });
 }
