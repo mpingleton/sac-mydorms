9f63f9abcce2914922dc6fa7c900fd5582336a29
Michael Pingleton <mnpingleton@gmail.com>
2021-10-02 13:11:29 -0500

    Added New Resident button.

--- frontend/src/features/residents/routes/Residents.jsx
+++ frontend/src/features/residents/routes/Residents.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { Button } from '@mui/material';
+
 import { ContentLayout } from '@/components/layout';
 
 import { ResidentList } from '../components/ResidentList';
 
 export const Residents = () => (
   <ContentLayout title="Residents">
+    <Button
+      variant="contained"
+      onClick={() => {}}
+    >
+      New Resident
+    </Button>
     <ResidentList />
   </ContentLayout>
 );
