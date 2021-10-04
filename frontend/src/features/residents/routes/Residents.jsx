ea59754e88807cf9d2308496a4a2b162f282c72d
Michael Pingleton <mnpingleton@gmail.com>
2021-10-04 14:42:01 -0500

    Added basic modal box.

--- frontend/src/features/residents/routes/Residents.jsx
+++ frontend/src/features/residents/routes/Residents.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
 
-import { Button } from '@mui/material';
+import { Box, Modal, Button } from '@mui/material';
 
 import { ContentLayout } from '@/components/layout';
 
 import { ResidentList } from '../components/ResidentList';
 
-export const Residents = () => (
-  <ContentLayout title="Residents">
-    <Button
-      variant="contained"
-      onClick={() => {}}
-    >
-      New Resident
-    </Button>
-    <ResidentList />
-  </ContentLayout>
-);
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export const Residents = () => {
+  const [modalOpen, setModalState] = React.useState(false);
+  const openModal = () => setModalState(true);
+  const closeModal = () => setModalState(false);
+
+  return (
+    <ContentLayout title="Residents">
+      <Modal
+        open={modalOpen}
+        onClose={() => {}}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          Hello!
+          <Button
+            variant="contained"
+            onClick={closeModal}
+          >
+            Cancel
+          </Button>
+        </Box>
+      </Modal>
+      <Button
+        variant="contained"
+        onClick={openModal}
+      >
+        New Resident
+      </Button>
+      <ResidentList />
+    </ContentLayout>
+  );
+};
 
 export default Residents;
