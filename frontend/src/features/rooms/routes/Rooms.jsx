38e9de217516660a23173e8366d6aeec50a2f33d
Michael Pingleton <mnpingleton@gmail.com>
2021-10-07 11:28:33 -0500

    Updated room view with stacks and added View and Assign buttons.

--- frontend/src/features/rooms/routes/Rooms.jsx
+++ frontend/src/features/rooms/routes/Rooms.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import { Button, Stack } from '@mui/material';
+
 import { ContentLayout } from '@/components/layout';
 
 import { RoomList } from '../components/RoomList';
 
 export const Rooms = () => (
   <ContentLayout title="Rooms">
-    <RoomList />
+    <Stack direction="column" spacing={1}>
+      <Stack direction="row" spacing={1}>
+        <Button
+          variant="contained"
+          onClick={() => {}}
+        >
+          View
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => {}}
+        >
+          Assign Resident
+        </Button>
+      </Stack>
+      <RoomList />
+    </Stack>
   </ContentLayout>
 );
 
