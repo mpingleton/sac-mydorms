15833b7ce8d0ab61c518d62d286a1a2d9b0a1131
Michael Pingleton <mnpingleton@gmail.com>
2021-10-08 12:19:33 -0500

    Added datagrid and personnel api call to modal.

--- frontend/src/features/rooms/components/AssignResidentDialog.jsx
+++ frontend/src/features/rooms/components/AssignResidentDialog.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Box, Modal, Stack } from '@mui/material';
+import { Button, Box, Modal, Stack, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+
 import getRoomById from '../api/getRoomById';
+import getResidents from '@/features/residents/api/getResidents';
 
 const modalStyle = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 600,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -18,16 +21,39 @@ const modalStyle = {
 
 export const AssignResidentDialog = ({ modalOpen, onClose, roomId }) => {
   const [room, setRoom] = React.useState({});
+  const [residents, setResidents] = React.useState([]);
+  const [selectedResidents, setSelectedResidents] = React.useState([]);
 
   React.useEffect(() => {
     if (room.id !== roomId && roomId > 0) {
       getRoomById(roomId).then((responseData) => setRoom(responseData));
     }
+    if (residents.length === 0) {
+      getResidents().then((responseData) => setResidents(responseData));
+    }
   });
 
-  if (room.id === undefined) {
+  if (room.id === undefined || residents.length === 0) {
     return null;
   }
+
+  const columns = [
+    { field: 'rank', headerName: 'Rank', width: 100 },
+    { field: 'first_name', headerName: 'First Name', width: 150 },
+    { field: 'middle_name', headerName: 'Middle Name', width: 150 },
+    { field: 'last_name', headerName: 'Last Name', width: 150 },
+  ];
+
+  const rows = residents.map((resident) => (
+    {
+      id: resident.id,
+      rank: resident.rank,
+      first_name: resident.first_name,
+      middle_name: resident.middle_name,
+      last_name: resident.last_name,
+    }
+  ));
+
   return (
     <Modal
       open={modalOpen}
@@ -37,7 +63,25 @@ export const AssignResidentDialog = ({ modalOpen, onClose, roomId }) => {
     >
       <Box sx={modalStyle}>
         <Stack direction="column" spacing={1}>
-          {room.room_number}
+          <Typography>
+            {`Select a resident to assign to room ${room.room_number}.`}
+          </Typography>
+          <Box sx={{ width: '100%', height: '300px' }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={20}
+              rowsPerPageOptions={[5]}
+              onSelectionModelChange={setSelectedResidents}
+              checkboxSelection
+            />
+          </Box>
+          <Stack direction="row">
+            <Button variant="contained" onClick={onClose}>Cancel</Button>
+            <Typography>
+              {`${selectedResidents.length} residents currently selected.`}
+            </Typography>
+          </Stack>
         </Stack>
       </Box>
     </Modal>
