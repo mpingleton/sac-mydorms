9c36dc4546ab4ec6c33adbca5b028a4b97b4a99b
Michael Pingleton <mnpingleton@gmail.com>
2021-10-14 12:55:25 -0500

    Added comments list to work order details modal.

--- frontend/src/features/work_orders/components/ViewWorkOrderDetailsDialog.jsx
+++ frontend/src/features/work_orders/components/ViewWorkOrderDetailsDialog.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Button, Box, Modal, Stack, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 
 import getWorkOrderById from '../api/getWorkOrderById';
+import getCommentsByWorkOrderId from '../api/getCommentsByWorkOrderId';
 
 const modalStyle = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 650,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -19,16 +21,35 @@ const modalStyle = {
 
 export const ViewWorkOrderDetailsDialog = ({ modalOpen, onClose, workOrderId }) => {
   const [workOrder, setWorkOrder] = React.useState({});
+  const [workOrderComments, setWorkOrderComments] = React.useState([]);
 
   React.useEffect(() => {
     if (workOrder.id !== workOrderId && workOrderId > 0) {
       getWorkOrderById(workOrderId).then((responseData) => setWorkOrder(responseData));
+      getCommentsByWorkOrderId(workOrderId).then(
+        (responseData) => setWorkOrderComments(responseData),
+      );
     }
   });
 
   if (workOrder.id === undefined) {
     return null;
   }
+
+  const commentColumns = [
+    { field: 'timestamp', headerName: 'Timestamp', width: 100 },
+    { field: 'by', headerName: 'By', width: 150 },
+    { field: 'comment', headerName: 'Comment', width: 390 },
+  ];
+
+  const commentRows = workOrderComments.map((comment) => (
+    {
+      id: comment.id,
+      by: comment.personnel_id,
+      timestamp: comment.timestamp,
+      comment: comment.comment,
+    }
+  ));
 
   return (
     <Modal
@@ -56,6 +77,18 @@ export const ViewWorkOrderDetailsDialog = ({ modalOpen, onClose, workOrderId }) 
           <Stack direction="row" spacing={1}>
             <Typography>Remarks:</Typography>
             <Typography>{workOrder.creator_remarks}</Typography>
+          </Stack>
+          <Stack direction="row" spacing={1}>
+            <Typography>Comments:</Typography>
+          </Stack>
+          <Stack direction="row" spacing={1} sx={{ height: 200 }}>
+            <DataGrid
+              rows={commentRows}
+              columns={commentColumns}
+              pageSize={20}
+              rowsPerPageOptions={[5]}
+              disableMultipleSelection
+            />
           </Stack>
           <Stack direction="row" spacing={1}>
             <Button variant="contained" onClick={onClose}>Close</Button>
