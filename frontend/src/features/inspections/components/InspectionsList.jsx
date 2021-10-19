e799a8d37f46f1ad9caf546086407728d174fcc9
Michael Pingleton <mnpingleton@gmail.com>
2021-10-19 10:41:21 -0500

    Added API call to front end to retrieve and display inspection data.

--- frontend/src/features/inspections/components/InspectionsList.jsx
+++ frontend/src/features/inspections/components/InspectionsList.jsx
@@ -3,15 +3,36 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
+import getRoomInspections from '../api/getRoomInspections';
+
 export const InspectionsList = () => {
+  const [roomInspections, setRoomInspections] = React.useState([]);
+
+  React.useEffect(() => {
+    if (roomInspections.length === 0) {
+      getRoomInspections().then((responseData) => setRoomInspections(responseData));
+    }
+  });
+
   const columns = [
+    { field: 'date', headerName: 'Date', width: 150 },
     { field: 'room', headerName: 'Room', width: 100 },
-    { field: 'subject', headerName: 'Subject', width: 200 },
-    { field: 'remarks', headerName: 'Creator Remarks', width: 300 },
-    { field: 'status', headerName: 'Status', width: 100 },
+    { field: 'resident', headerName: 'Resident', width: 200 },
+    { field: 'dorm_manager', headerName: 'Dorm Manager', width: 200 },
+    { field: 'inspector', headerName: 'Inspector', width: 200 },
   ];
 
-  const rows = [];
+  const rows = roomInspections.map((inspection) => (
+    {
+      id: inspection.id,
+      date: inspection.timestamp,
+      room: inspection.room_id,
+      resident: inspection.personnel_id,
+      dorm_manager: inspection.dorm_manager_id,
+      inspector: inspection.inspector_name,
+    }
+  ));
+
   return (
     <Box sx={{ height: '400px', width: '100%' }}>
       <DataGrid
