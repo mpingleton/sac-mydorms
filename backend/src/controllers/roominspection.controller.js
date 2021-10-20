00ac441943450be18f6195aa906b1be80f08c6dd
Michael Pingleton <mnpingleton@gmail.com>
2021-10-20 13:31:11 -0500

    Added room object and personnel object to getting room inspection.

--- backend/src/controllers/roominspection.controller.js
+++ backend/src/controllers/roominspection.controller.js
@@ -2,9 +2,27 @@ const { ExtractJwt } = require('passport-jwt');
 
 const { authService } = require('@/services');
 const roomInspectionService = require('@/services/roominspection.service');
+const roomService = require('@/services/room.service');
+const personnelService = require('@/services/personnel.service');
 
 const getRoomInspections = async (req, res) => {
   const roomInspections = await roomInspectionService.getRoomInspections();
+  const promises = [];
+  for (let i = 0; i < roomInspections.length; i += 1) {
+    promises.push(roomService.getRoomById(roomInspections[i].room_id)
+      .then((roomObject) => {
+        roomInspections[i].roomObject = roomObject;
+      }));
+    promises.push(personnelService.getPersonnelById(roomInspections[i].personnel_id)
+      .then((personnelObject) => {
+        roomInspections[i].personnelObject = personnelObject;
+      }));
+    promises.push(personnelService.getPersonnelById(roomInspections[i].dorm_manager_id)
+      .then((personnelObject) => {
+        roomInspections[i].dormManagerPersonnelObject = personnelObject;
+      }));
+  }
+  await Promise.all(promises);
   res.send(200, roomInspections);
 };
 
