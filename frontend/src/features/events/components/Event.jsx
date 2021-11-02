9b8603a1791e374cf0946a93903d11b24a9d2d27
Michael Pingleton <mnpingleton@gmail.com>
2021-11-02 10:58:07 -0500

    Added dummy data to the event list.

--- frontend/src/features/events/components/Event.jsx
+++ frontend/src/features/events/components/Event.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Typography, Card, CardContent } from '@mui/material';
 
-export const Event = () => (
+export const Event = ({ eventObject }) => (
   <Card variant="outlined">
     <CardContent>
-      <Typography>JH</Typography>
+      <Typography>{eventObject.subject}</Typography>
+      <Typography>{eventObject.scheduled}</Typography>
+      <Typography>{eventObject.description}</Typography>
     </CardContent>
   </Card>
 );
+
+Event.propTypes = {
+  eventObject: PropTypes.shape(
+    {
+      id: PropTypes.number,
+      created_by: PropTypes.number,
+      scheduled: PropTypes.string,
+      location: PropTypes.string,
+      subject: PropTypes.string,
+      description: PropTypes.string,
+    },
+  ),
+};
+
+Event.defaultProps = {
+  eventObject: {
+    id: 0,
+    created_by: 0,
+    scheduled: new Date().toISOString(),
+    location: '',
+    subject: '',
+    description: '',
+  },
+};
 
 export default Event;
--- frontend/src/features/events/components/EventList.jsx
+++ frontend/src/features/events/components/EventList.jsx
@@ -4,21 +4,55 @@ import { Stack } from '@mui/material';
 
 import { Event } from './Event';
 
-export const EventList = () => (
-  <Stack
-    direction="column"
-    spacing={4}
-    sx={{
-      minWidth: 350,
-      maxWidth: '95vw',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      alignSelf: 'center',
-    }}
-  >
-    <Event />
-    <Event />
-  </Stack>
-);
+export const EventList = () => {
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    const data = [
+      {
+        id: 1,
+        created_by: 1,
+        scheduled: new Date().toISOString(),
+        location: 'Ezekiel Hall Lobby',
+        subject: 'Cookout',
+        description: 'FSS will be hosting a cookout in the lobby of Ezekiel Hall!',
+      },
+      {
+        id: 2,
+        created_by: 1,
+        scheduled: new Date().toISOString(),
+        location: 'Tony Hall Lobby',
+        subject: 'Test Event',
+        description: 'This is another test event, but this time, in Tony Hall!',
+      },
+      {
+        id: 3,
+        created_by: 1,
+        scheduled: new Date().toISOString(),
+        location: 'Tony Hall Lobby',
+        subject: 'Another Test Event',
+        description: 'This is yet another test event, but this time, in Tony Hall!',
+      },
+    ];
+
+    setEvents(data);
+  }, []);
+
+  return (
+    <Stack
+      direction="column"
+      spacing={4}
+      sx={{
+        minWidth: 350,
+        maxWidth: '95vw',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        alignSelf: 'center',
+      }}
+    >
+      {events.map((event) => (<Event eventObject={event} />))}
+    </Stack>
+  );
+};
 
 export default EventList;
