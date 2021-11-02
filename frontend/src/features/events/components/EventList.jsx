020a642e93389ee8455d1c1d78df2e1b837308c3
Michael Pingleton <mnpingleton@gmail.com>
2021-11-02 09:58:25 -0500

    Fixed formatting.

--- frontend/src/features/events/components/EventList.jsx
+++ frontend/src/features/events/components/EventList.jsx
@@ -5,7 +5,17 @@ import { Stack } from '@mui/material';
 import { Event } from './Event';
 
 export const EventList = () => (
-  <Stack direction="column" spacing={4} sx={{ minWidth: 350, maxWidth: '95vw', marginLeft: 'auto', marginRight: 'auto', alignSelf: 'center' }}>
+  <Stack
+    direction="column"
+    spacing={4}
+    sx={{
+      minWidth: 350,
+      maxWidth: '95vw',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      alignSelf: 'center',
+    }}
+  >
     <Event />
     <Event />
   </Stack>
