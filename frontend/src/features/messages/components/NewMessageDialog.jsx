30ba8bfc3ff277037809bad7fd059c3f9f73363f
Michael Pingleton <mnpingleton@gmail.com>
2021-11-09 11:00:21 -0600

    Added state hooks

--- frontend/src/features/messages/components/NewMessageDialog.jsx
+++ frontend/src/features/messages/components/NewMessageDialog.jsx
@@ -15,54 +15,63 @@ const style = {
   p: 4,
 };
 
-export const NewMessageDialog = ({ modalOpen, onClose }) => (
-  <Modal
-    open={modalOpen}
-    onClose={onClose}
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description"
-  >
-    <Box sx={style}>
-      <Stack direction="column" spacing={1}>
-        <Select
-          label="To"
-          onChange={() => {}}
-        >
-          <MenuItem value={0} disabled>
-            <em>Please select a person to send this message to...</em>
-          </MenuItem>
-        </Select>
-        <TextField
-          label="Subject"
-          variant="outlined"
-          onChange={() => {}}
-        />
-        <TextField
-          label="Body"
-          variant="outlined"
-          multiline
-          rows={4}
-          onChange={() => {}}
-        />
-        <Stack direction="row" spacing={1}>
-          <Button
-            variant="contained"
-            onClick={() => onClose()}
+export const NewMessageDialog = ({ modalOpen, onClose }) => {
+  const [resSubject, setSubject] = React.useState('');
+  const [resMessageBody, setMessageBody] = React.useState('');
+
+  const sendMessage = () => {
+    console.log(resSubject, resMessageBody);
+  };
+
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Stack direction="column" spacing={1}>
+          <Select
+            label="To"
+            onChange={() => {}}
           >
-            Cancel
-          </Button>
-          <Button
-            variant="contained"
-            disabled
-            onClick={() => {}}
-          >
-            Send
-          </Button>
+            <MenuItem value={0} disabled>
+              <em>Please select a person to send this message to...</em>
+            </MenuItem>
+          </Select>
+          <TextField
+            label="Subject"
+            variant="outlined"
+            onChange={(event) => setSubject(event.target.value)}
+          />
+          <TextField
+            label="Body"
+            variant="outlined"
+            multiline
+            rows={4}
+            onChange={(event) => setMessageBody(event.target.value)}
+          />
+          <Stack direction="row" spacing={1}>
+            <Button
+              variant="contained"
+              onClick={() => onClose()}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              disabled
+              onClick={() => sendMessage()}
+            >
+              Send
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
-  </Modal>
-);
+      </Box>
+    </Modal>
+  );
+};
 
 NewMessageDialog.propTypes = {
   modalOpen: PropTypes.func,
