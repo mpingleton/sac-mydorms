11eb63fffb52f73d58ba13b8b3f025c0a023e72b
Michael Pingleton <mnpingleton@gmail.com>
2021-11-17 10:17:43 -0600

    Added new set of seed data.  There will be more seed data added to this file in the future.

--- backend/src/db/seed.js
+++ backend/src/db/seed.js
@@ -5,287 +5,45 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const userData = [
+const users = [
   {
-    name: 'Alice',
     email: 'alice@prisma.io',
     password: '123',
-    isEmailVerified: false,
-    role: 'ADMIN',
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    password: '123',
+    role: 'USER',
+    name: 'Alice',
     isEmailVerified: false,
   },
   {
-    name: 'Mahmoud',
     email: 'mahmoud@prisma.io',
     password: '123',
+    role: 'USER',
+    name: 'Mahmoud',
     isEmailVerified: false,
   },
   {
-    name: 'Tony',
-    email: 'tony.doe@us.af.mil',
+    email: 'tony@prisma.io',
     password: '123',
+    role: 'USER',
+    name: 'Tony',
     isEmailVerified: false,
   },
   {
-    name: 'Mike',
-    email: 'mike@prisma.io',
-    password: 'test1234',
+    email: 'ezekiel@prisma.io',
+    password: '123',
+    role: 'USER',
+    name: 'Ezekiel',
+    isEmailVerified: false,
+  },
+  {
+    email: 'john.snuffy@mydorms.com',
+    password: '12345',
+    role: 'USER',
+    name: 'John',
     isEmailVerified: false,
   },
 ];
 
-const buildingsData = [
-  {
-    base_id: 1,
-    building_number: '123',
-    building_name: 'Tony Manor',
-    address: '123 Main St, Barksdale AFB 71037',
-  },
-  {
-    base_id: 1,
-    building_number: '456',
-    building_name: 'Ezekiel Manor',
-    address: '456 Main St, Barksdale AFB 71037',
-  },
-];
-
-const roomsData = [
-
-  {
-    building_id: 1,
-    room_number: '1A',
-    status: 1,
-  },
-  {
-    building_id: 1,
-    room_number: '1B',
-    status: 1,
-  },
-  {
-    building_id: 1,
-    room_number: '1C',
-    status: 1,
-  },
-  {
-    building_id: 1,
-    room_number: '1D',
-    status: 0,
-  },
-
-  {
-    building_id: 1,
-    room_number: '2A',
-    status: 1,
-  },
-  {
-    building_id: 1,
-    room_number: '2B',
-    status: 1,
-  },
-  {
-    building_id: 1,
-    room_number: '2C',
-    status: 1,
-  },
-  {
-    building_id: 1,
-    room_number: '2D',
-    status: 0,
-  },
-
-  {
-    building_id: 2,
-    room_number: '1A',
-    status: 1,
-  },
-  {
-    building_id: 2,
-    room_number: '1B',
-    status: 1,
-  },
-  {
-    building_id: 2,
-    room_number: '1C',
-    status: 1,
-  },
-  {
-    building_id: 2,
-    room_number: '1D',
-    status: 0,
-  },
-
-  {
-    building_id: 2,
-    room_number: '2A',
-    status: 1,
-  },
-  {
-    building_id: 2,
-    room_number: '2B',
-    status: 1,
-  },
-  {
-    building_id: 2,
-    room_number: '2C',
-    status: 1,
-  },
-  {
-    building_id: 2,
-    room_number: '2D',
-    status: 0,
-  },
-
-];
-
-const personnelData = [
-  {
-    base_id: 1,
-    rank: 'AB',
-    first_name: 'Tony',
-    middle_name: 'Randy',
-    last_name: 'Doe',
-    phone: '123-456-7891',
-    email: 'tony.doe.1@us.af.mil',
-    is_dorm_manager: false,
-  },
-  {
-    base_id: 1,
-    rank: 'A1C',
-    first_name: 'James',
-    middle_name: 'Brad',
-    last_name: 'Mosely',
-    phone: '123-456-7891',
-    email: 'james.mosely.10@us.af.mil',
-    is_dorm_manager: false,
-  },
-  {
-    base_id: 1,
-    rank: 'SSgt',
-    first_name: 'John',
-    middle_name: 'Daniel',
-    last_name: 'Snuffy',
-    phone: '123-456-7891',
-    email: 'john.snuffy@us.af.mil',
-    is_dorm_manager: false,
-  },
-];
-
-const enrollmentsData = [
-  {
-    user_id: 4,
-    personnel_id: 1,
-  },
-];
-
-const pendingEnrollmentsData = [
-  {
-    personnel_id: 3,
-    registration_code: '5X5HHA5',
-  },
-];
-
-const roomAssignmentsData = [
-  {
-    personnel_id: 1,
-    room_id: 1,
-  },
-  {
-    personnel_id: 2,
-    room_id: 11,
-  },
-];
-
-const workOrdersData = [
-  {
-    room_id: 1,
-    subject: 'Broken Sink',
-    created_by: 3,
-    creator_remarks: 'Sink is leaking water into the cabinet below.',
-    created_timestamp: '2021-09-20T00:00:00.000Z',
-    status: 1,
-    status_timestamp: '2021-09-20T00:00:00.000Z',
-  },
-  {
-    room_id: 11,
-    subject: 'Broken A/C.',
-    created_by: 2,
-    creator_remarks: 'Air conditioner is blowing hot air.',
-    created_timestamp: '2021-09-20T00:00:00.000Z',
-    status: 1,
-    status_timestamp: '2021-09-20T00:00:00.000Z',
-  },
-];
-
-const workOrderCommentsData = [
-  {
-    work_order_id: 1,
-    personnel_id: 3,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    comment: 'Water damage was found near the sink from leakage.',
-  },
-  {
-    work_order_id: 1,
-    personnel_id: 3,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    comment: 'CE will need to cut out some of the drywall near the sink due to water damage.',
-  },
-  {
-    work_order_id: 2,
-    personnel_id: 3,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    comment: 'Thermostat was found on the heat setting.  Closing work order.',
-  },
-];
-
-const roomInspectionsData = [
-  {
-    room_id: 2,
-    resident_id: 2,
-    dorm_manager_id: 3,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    inspector_name: 'MSgt Inspector',
-    inspector_remarks: 'No discrepancy.',
-  },
-  {
-    room_id: 1,
-    resident_id: 1,
-    dorm_manager_id: 3,
-    timestamp: '2021-10-20T00:00:00.000Z',
-    inspector_name: 'MSgt Inspector',
-    inspector_remarks: 'Dirty laundry all over the place.',
-  },
-];
-
-const messagesData = [
-  {
-    sender_id: 3,
-    recipient_id: 1,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    subject: 'BAH Concerns',
-    body: 'Sir,\n\nI need a copy of your LES in order to get your BAH authorization going.',
-  },
-  {
-    sender_id: 3,
-    recipient_id: 2,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    subject: 'A/C Work Order',
-    body: 'Sir,\n\nI am currently working on the work order for your air conditioning system.',
-  },
-  {
-    sender_id: 3,
-    recipient_id: 2,
-    timestamp: '2021-09-20T00:00:00.000Z',
-    subject: 'A/C Work Order Progress',
-    body: 'Sir,\n\nWe found mold inside the air handler, you may need to change rooms.',
-  },
-];
-
-const basesData = [
+const bases = [
   {
     name: 'Whiteman AFB',
   },
@@ -300,100 +58,438 @@ const basesData = [
   },
 ];
 
+const personnel = [
+  {
+    base_id: 1,
+    rank: 'SrA',
+    first_name: 'Tony',
+    middle_name: 'Man',
+    last_name: 'Doe',
+    phone: '123-456-7890',
+    email: 'tony@prisma.io',
+    is_dorm_manager: false,
+  },
+  {
+    base_id: 1,
+    rank: 'A1C',
+    first_name: 'Alice',
+    middle_name: 'Emma',
+    last_name: 'Doe',
+    phone: '123-456-7890',
+    email: 'alice@prisma.io',
+    is_dorm_manager: false,
+  },
+  {
+    base_id: 1,
+    rank: 'AB',
+    first_name: 'Ezekiel',
+    middle_name: 'Tyler',
+    last_name: 'Gary',
+    phone: '123-456-7890',
+    email: 'ezekiel@prisma.io',
+    is_dorm_manager: false,
+  },
+  {
+    base_id: 1,
+    rank: 'SSgt',
+    first_name: 'John',
+    middle_name: 'Tyler',
+    last_name: 'Snuffy',
+    phone: '123-456-7890',
+    email: 'john.snuffy@mydorms.com',
+    is_dorm_manager: true,
+  },
+];
+
+const enrollments = [
+  {
+    user_id: 1,
+    personnel_id: 2,
+  },
+  {
+    user_id: 4,
+    personnel_id: 3,
+  },
+  {
+    user_id: 5,
+    personnel_id: 4,
+  },
+];
+
+const pendingEnrollments = [
+  {
+    personnel_id: 1,
+    registration_code: 'A5AJX',
+  },
+];
+
+const buildings = [
+  {
+    base_id: 1,
+    building_number: '120',
+    building_name: 'Columbia Hall',
+    address: '120 Main St',
+  },
+  {
+    base_id: 1,
+    building_number: '121',
+    building_name: 'Discovery Hall',
+    address: '121 Main St',
+  },
+  {
+    base_id: 2,
+    building_number: '222',
+    building_name: 'Tony Hall',
+    address: '222 Main St',
+  },
+  {
+    base_id: 2,
+    building_number: '223',
+    building_name: 'Ezekiel Hall',
+    address: '223 Main St',
+  },
+  {
+    base_id: 3,
+    building_number: '666',
+    building_name: 'Smith Manor',
+    address: '666 Triangle St',
+  },
+  {
+    base_id: 3,
+    building_number: '667',
+    building_name: 'Holbrook Manor',
+    address: '667 Triangle St',
+  },
+  {
+    base_id: 4,
+    building_number: '111',
+    building_name: 'Jane Hall',
+    address: '111 Main St',
+  },
+  {
+    base_id: 4,
+    building_number: '112',
+    building_name: 'John Hall',
+    address: '112 Main St',
+  },
+];
+
+const rooms = [
+  {
+    building_id: 1,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 1,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 1,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 1,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 2,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 2,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 2,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 2,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 3,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 3,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 3,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 3,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 4,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 4,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 4,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 4,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 5,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 5,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 5,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 5,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 6,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 6,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 6,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 6,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 7,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 7,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 7,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 7,
+    room_number: '2B',
+    status: 1,
+  },
+  {
+    building_id: 8,
+    room_number: '1A',
+    status: 1,
+  },
+  {
+    building_id: 8,
+    room_number: '1B',
+    status: 1,
+  },
+  {
+    building_id: 8,
+    room_number: '2A',
+    status: 0,
+  },
+  {
+    building_id: 8,
+    room_number: '2B',
+    status: 1,
+  },
+];
+
+const roomAssignments = [
+  {
+    personnel_id: 2,
+    room_id: 1,
+  },
+];
+
+const workOrders = [
+  {
+    room_id: 1,
+    subject: 'A/C is broken',
+    creator_remarks: 'A/C is blowing hot air.',
+    created_by: 1,
+    created_timestamp: new Date().toISOString(),
+    status: 1,
+    status_timestamp: new Date().toISOString(),
+  },
+  {
+    room_id: 1,
+    subject: 'Sink Leaking',
+    creator_remarks: 'Sink wont stop leaking.',
+    created_by: 1,
+    created_timestamp: new Date().toISOString(),
+    status: 1,
+    status_timestamp: new Date().toISOString(),
+  },
+];
+
+const workOrderComments = [];
+
+const roomInspections = [
+  {
+    room_id: 1,
+    resident_id: 1,
+    dorm_manager_id: 3,
+    timestamp: new Date().toISOString(),
+    inspector_name: 'SSgt Jane Doe',
+    inspector_remarks: 'No discrepancy.',
+  },
+];
+
+const events = [
+  {
+    base_id: 1,
+    created_by: 1,
+    scheduled: new Date().toISOString(),
+    location: 'Fitness Center',
+    subject: 'Jiu Jit Tsu Practice',
+    description: 'Hello all!\n\nWe are holding a Jiu Jit Tsu get together!  Please come and join us if you can.',
+  },
+];
+
+const eventResponses = [];
+
+const commonAreaPosts = [
+  {
+    base_id: 1,
+    posted_by: 4,
+    timestamp: new Date().toISOString(),
+    text: 'Hello world! This is an example of a common area post.',
+    comments_enabled: true,
+  },
+];
+
+const commonAreaPostComments = [];
+
+const messages = [];
+
 async function main() {
-  console.log('Start seeding ...');
+  console.log('Start seeding...');
 
-  // Seed users table.
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
+  for (const user of users) {
+    const newUser = await prisma.user.create({ data: user });
+    console.log(`New user: ${JSON.stringify(newUser)}.`);
+  }
+
+  for (const base of bases) {
+    const newBase = await prisma.bases.create({ data: base });
+    console.log(`New base: ${JSON.stringify(newBase)}.`);
+  }
+
+  for (const person of personnel) {
+    const newPerson = await prisma.personnel.create({ data: person });
+    console.log(`New person: ${JSON.stringify(newPerson)}.`);
+  }
+
+  for (const enrollment of enrollments) {
+    const newEnrollment = await prisma.enrollments.create({ data: enrollment });
+    console.log(`New enrollment: ${JSON.stringify(newEnrollment)}.`);
+  }
+
+  for (const pendingEnrollment of pendingEnrollments) {
+    const newPendingEnrollment = await prisma.pendingEnrollments.create(
+      { data: pendingEnrollment },
+    );
+    console.log(`New pending enrollment: ${JSON.stringify(newPendingEnrollment)}.`);
+  }
+
+  for (const building of buildings) {
+    const newBuilding = await prisma.buildings.create({ data: building });
+    console.log(`New building: ${JSON.stringify(newBuilding)}.`);
+  }
+
+  for (const room of rooms) {
+    const newRoom = await prisma.rooms.create({ data: room });
+    console.log(`New room: ${JSON.stringify(newRoom)}.`);
+  }
+
+  for (const roomAssignment of roomAssignments) {
+    const newRoomAssignment = await prisma.roomAssignments.create({ data: roomAssignment });
+    console.log(`New room assignment: ${JSON.stringify(newRoomAssignment)}.`);
+  }
+
+  for (const workOrder of workOrders) {
+    const newWorkOrder = await prisma.workOrders.create({ data: workOrder });
+    console.log(`New work order: ${JSON.stringify(newWorkOrder)}.`);
+  }
+
+  for (const workOrderComment of workOrderComments) {
+    const newWorkOrderComment = await prisma.workOrderComments.create({ data: workOrderComment });
+    console.log(`New work order comment: ${JSON.stringify(newWorkOrderComment)}.`);
+  }
+
+  for (const roomInspection of roomInspections) {
+    const newRoomInspection = await prisma.roomInspections.create({ data: roomInspection });
+    console.log(`New room inspection: ${JSON.stringify(newRoomInspection)}.`);
+  }
+
+  for (const evnt of events) {
+    const newEvent = await prisma.events.create({ data: evnt });
+    console.log(`New event: ${JSON.stringify(newEvent)}.`);
+  }
+
+  for (const eventResponse of eventResponses) {
+    const newEventResponse = await prisma.eventResponses.create({ data: eventResponse });
+    console.log(`New event response: ${JSON.stringify(newEventResponse)}.`);
+  }
+
+  for (const commonAreaPost of commonAreaPosts) {
+    const newCommonAreaPost = await prisma.commonAreaPosts.create({ data: commonAreaPost });
+    console.log(`New common area post: ${JSON.stringify(newCommonAreaPost)}.`);
+  }
+
+  for (const commonAreaPostComment of commonAreaPostComments) {
+    const newCommonAreaPostComment = await prisma.commonAreaPostComments.create({
+      data: commonAreaPostComment,
     });
-    console.log(`Created user ${user.name} with id: ${user.id}.`);
+    console.log(`New common area post comment: ${JSON.stringify(newCommonAreaPostComment)}.`);
   }
 
-  // Seed the base data.
-  for (const b of basesData) {
-    const base = await prisma.bases.create({ data: b });
-    console.log(`Create base ${base.name} with an ID of ${base.id}.`);
-  }
-
-  // Seed buildings table.
-  for (const b of buildingsData) {
-    const building = await prisma.buildings.create({
-      data: b,
-    });
-    console.log(`Created building ${building.building_number} with name ${building.building_name}.`);
-  }
-
-  // Seed rooms table.
-  for (const r of roomsData) {
-    const room = await prisma.rooms.create({
-      data: r,
-    });
-    console.log(`Created room ${room.room_number}.`);
-  }
-
-  // Seed personnel table.
-  for (const p of personnelData) {
-    const person = await prisma.personnel.create({
-      data: p,
-    });
-    console.log(`Created person ${person.rank} ${person.first_name} ${person.middle_name} ${person.last_name}.`);
-  }
-
-  // Seed enrollments data.
-  for (const e of enrollmentsData) {
-    const enrollment = await prisma.enrollments.create({
-      data: e,
-    });
-    console.log(`Created enrollment of user ${enrollment.user_id} to person ${enrollment.personnel_id}.`);
-  }
-
-  for (const e of pendingEnrollmentsData) {
-    const pendingEnrollment = await prisma.pendingEnrollments.create({
-      data: e,
-    });
-    console.log(`Created pending enrollment for personnel id
-      ${pendingEnrollment.personnel_id}
-      with a registration code
-      ${pendingEnrollment.registration_code}
-    .`);
-  }
-
-  // Seed room assignments table.
-  for (const ra of roomAssignmentsData) {
-    const roomAssignment = await prisma.roomAssignments.create({
-      data: ra,
-    });
-    console.log(`Assigned person id ${roomAssignment.personnel_id} to room id: ${roomAssignment.room_id}.`);
-  }
-
-  // Seed work orders table.
-  for (const wo of workOrdersData) {
-    const workOrder = await prisma.workOrders.create({
-      data: wo,
-    });
-    console.log(`Work order ${workOrder.creator_remarks} created.`);
-  }
-
-  // Seed work order comments table.
-  for (const wc of workOrderCommentsData) {
-    const workOrderComment = await prisma.workOrderComments.create({
-      data: wc,
-    });
-    console.log(`Created new work order comment: ${workOrderComment.comment}.`);
-  }
-
-  // Seed the inspections table.
-  for (const insp of roomInspectionsData) {
-    const inspection = await prisma.roomInspections.create({ data: insp });
-    console.log(`Created room inspection with the following remarks: ${inspection.inspector_remarks}.`);
-  }
-
-  // Seed the messages table.
-  for (const mess of messagesData) {
-    const message = await prisma.messages.create({ data: mess });
-    console.log(`Created message from ${message.sender_id} to ${message.recipient_id} with a subject of ${message.subject}: ${message.body}.`);
+  for (const message of messages) {
+    const newMessage = await prisma.messages.create({ data: message });
+    console.log(`New message: ${newMessage}.`);
   }
 
   console.log('Seeding finished.');
