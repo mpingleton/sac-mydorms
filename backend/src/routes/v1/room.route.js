4c16f8e75f5a94c1cc3697db544c9c819e6d578f
Michael Pingleton <mnpingleton@gmail.com>
2021-11-24 14:06:24 -0600

    Added gatekeeper to room route.

--- backend/src/routes/v1/room.route.js
+++ backend/src/routes/v1/room.route.js
@@ -4,18 +4,21 @@ const auth = require('@/middlewares/auth');
 const validate = require('@/middlewares/validate');
 const roomController = require('@/controllers/room.controller');
 const roomValidation = require('@/validations/room.validation');
+const dormManagerGatekeeper = require('@/gatekeepers/dormmanager.gatekeeper');
 
 const router = express.Router();
 
 router.get(
   '/',
   auth(),
+  dormManagerGatekeeper.isDormManager,
   roomController.getRooms,
 );
 
 router.get(
   '/:id',
   auth(),
+  dormManagerGatekeeper.isDormManager,
   validate(roomValidation.getRoomById),
   roomController.getRoomById,
 );
