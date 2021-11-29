6a42aeecb2ac5975c1d22732c0aa49a6ab340e7d
Michael Pingleton <mnpingleton@gmail.com>
2021-11-28 21:46:42 -0600

    Added input validation using Joi to the registration page.

--- frontend/src/features/auth/components/RegisterForm.jsx
+++ frontend/src/features/auth/components/RegisterForm.jsx
@@ -6,10 +6,20 @@ import Button from '@mui/material/Button';
 
 import registerCall from '../api/register';
 
+const Joi = require('joi');
+
 export const RegisterForm = ({ onSuccess, onCancel }) => {
   const [resUsername, setUsername] = React.useState('');
   const [resPassword, setPassword] = React.useState('');
   const [resRegCode, setRegCode] = React.useState('');
+
+  const usernameValidation = Joi.string().min(3).max(60).required()
+    .validate(resUsername);
+  const passwordValidation = Joi.string().min(8).required()
+    .validate(resPassword);
+  const regCodeValidation = Joi.string().alphanum().min(10).max(10)
+    .required()
+    .validate(resRegCode);
 
   const submit = () => {
     const data = {
@@ -26,17 +36,20 @@ export const RegisterForm = ({ onSuccess, onCancel }) => {
       <TextField
         type="text"
         label="Username"
+        error={usernameValidation.error && resUsername.length > 0}
         fullWidth
         onChange={(event) => setUsername(event.target.value)}
       />
       <TextField
         type="password"
         label="Password"
+        error={passwordValidation.error && resPassword.length > 0}
         fullWidth
         onChange={(event) => setPassword(event.target.value)}
       />
       <TextField
         label="Registration Code"
+        error={regCodeValidation.error && resRegCode.length > 0}
         fullWidth
         onChange={(event) => setRegCode(event.target.value)}
       />
@@ -51,6 +64,11 @@ export const RegisterForm = ({ onSuccess, onCancel }) => {
         <Button
           variant="contained"
           onClick={() => { submit(); }}
+          disabled={
+            usernameValidation.error
+            || passwordValidation.error
+            || regCodeValidation.error
+          }
           fullWidth
         >
           Register
