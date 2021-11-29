b70a56ad7833b074b6680b5abfff668b1bfd44a4
SAC User '21 <sacuser21@CEs-MacBook-Air.local>
2021-11-29 12:36:06 -0600

    Added tables to Dashboard features on work orders and inspections

--- frontend/src/features/misc/components/WorkOrder.jsx
+++ frontend/src/features/misc/components/WorkOrder.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Typography, Card, CardContent } from '@mui/material';
+import { Typography, Card, CardContent, Table, TableRow, TableCell, TableHead, TableBody } from '@mui/material';
+
+function createData(workOrder, progress, time) {
+  return { workOrder, progress, time };
+}
+
+const rows = [
+  createData('Sink', 'Open', '16:45'),
+  createData('Air Conditioner', 'Stalled', '8:30'),
+  createData('Water Leak', 'In Progress', '10:45'),
+  createData('Door Lock', 'Stalled', '13:30'),
+];
 
 export const WorkOrder = () => (
   <Card
@@ -8,6 +19,29 @@ export const WorkOrder = () => (
   >
     <CardContent>
       <Typography>Work Orders</Typography>
+      <Table sx={{ minWidth: 500 }} aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Work Order</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.workOrder}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" style={{ width: 150 }}>
+                {row.workOrder}
+              </TableCell>
+              <TableCell>{row.progress}</TableCell>
+              <TableCell>{row.time}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </CardContent>
   </Card>
 );
