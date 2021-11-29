15f6fd509554993b2e2763ba7044d2c7ca9e39fe
SAC User '21 <sacuser21@CEs-MacBook-Air.local>
2021-11-29 12:36:06 -0600

    Added tables to Dashboard features on work orders and inspections

--- frontend/src/features/misc/components/Inspection.jsx
+++ frontend/src/features/misc/components/Inspection.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Typography, Card, CardContent } from '@mui/material';
+import { Typography, Card, CardContent, Table, TableRow, TableCell, TableHead, TableBody } from '@mui/material';
+
+function createData(inspections, results, time) {
+  return { inspections, results, time };
+}
+
+const rows = [
+  createData('Room 1', 'Pass', '8:30'),
+  createData('Room 2', 'Fail', '15.45'),
+  createData('Room 3', 'Pass', '13:30'),
+];
 
 export const Inspection = () => (
   <Card
@@ -8,6 +18,29 @@ export const Inspection = () => (
   >
     <CardContent>
       <Typography>Inspections</Typography>
+      <Table sx={{ minWidth: 500 }} aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Inspection</TableCell>
+            <TableCell align="left">Result</TableCell>
+            <TableCell align="left">Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.inspections}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" style={{ width: 150 }}>
+                {row.inspections}
+              </TableCell>
+              <TableCell>{row.results}</TableCell>
+              <TableCell>{row.time}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </CardContent>
   </Card>
 );
