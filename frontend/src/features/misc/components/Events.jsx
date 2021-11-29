153aa0b569757865b373f0c83589415d40abdc59
SAC User '21 <sacuser21@CEs-MacBook-Air.local>
2021-11-29 13:06:33 -0600

    Added Event dashboard feature to Events card

--- frontend/src/features/misc/components/Events.jsx
+++ frontend/src/features/misc/components/Events.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Typography, Card, CardContent } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 
+const columns = [
+  { field: 'id', headerName: 'Event Name', width: 90, editable: true },
+  {
+    field: 'Date',
+    headerName: 'Date',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'Time',
+    headerName: 'Time',
+    width: 150,
+    editable: true,
+  },
+];
+
+const rows = [
+  { id: 'Dance', Date: '12/01', Time: '17:30' },
+  { id: 'Spring Jammer', Date: '01/29', Time: '6:00' },
+  { id: 'Sports Tournament', Date: '02/24', Time: '15:45' },
+  { id: 'Pot Luck', Date: '03/15', Time: '18:00' },
+];
 export const Events = () => (
   <Card
-    sx={{ minHeight: 200, minWidth: 400 }}
+    sx={{ minHeight: 400, minWidth: 600 }}
     variant="outlined"
   >
     <CardContent>
       <Typography>Events</Typography>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+      />
     </CardContent>
   </Card>
 );
