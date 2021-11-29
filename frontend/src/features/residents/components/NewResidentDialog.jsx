faf997cdfdd0d5ca438bec546730c2ea939bc31d
Michael Pingleton <mnpingleton@gmail.com>
2021-11-28 19:12:44 -0600

    Added front end validation (using the Joi library) to the residents page dialogs.

--- frontend/src/features/residents/components/NewResidentDialog.jsx
+++ frontend/src/features/residents/components/NewResidentDialog.jsx
@@ -5,6 +5,8 @@ import { Box, Modal, Button, TextField, Stack, Typography } from '@mui/material'
 
 import createPersonnel from '@/api/createPersonnel';
 
+const Joi = require('joi');
+
 const modalStyle = {
   position: 'absolute',
   top: '50%',
@@ -24,6 +26,19 @@ export const NewResidentDialog = ({ modalOpen, onClose }) => {
   const [resLastName, setLastName] = React.useState('');
   const [resEmail, setEmail] = React.useState('');
   const [resPhone, setPhone] = React.useState('');
+
+  const rankValidation = Joi.string().min(2).max(6).required()
+    .validate(resRank);
+  const firstNameValidation = Joi.string().min(1).max(150).required()
+    .validate(resFirstName);
+  const middleNameValidation = Joi.string().min(1).max(150).required()
+    .validate(resMiddleName);
+  const lastNameValidation = Joi.string().min(1).max(150).required()
+    .validate(resLastName);
+  const emailValidation = Joi.string().min(1).email({ tlds: { allow: false } }).required()
+    .validate(resEmail);
+  const phoneValidation = Joi.string().min(1).max(15).required()
+    .validate(resPhone);
 
   const submitResident = () => {
     const data = {
@@ -53,42 +68,61 @@ export const NewResidentDialog = ({ modalOpen, onClose }) => {
           <TextField
             id="new-resident-rank"
             label="Rank"
+            error={rankValidation.error && resRank.length > 0}
             variant="standard"
             onChange={(event) => { setRank(event.target.value); }}
           />
           <TextField
             id="new-resident-first-name"
             label="First Name"
+            error={firstNameValidation.error && resFirstName.length > 0}
             variant="standard"
             onChange={(event) => { setFirstName(event.target.value); }}
           />
           <TextField
             id="new-resident-middle-name"
             label="Middle Name"
+            error={middleNameValidation.error && resMiddleName.length > 0}
             variant="standard"
             onChange={(event) => { setMiddleName(event.target.value); }}
           />
           <TextField
             id="new-resident-last-name"
             label="Last Name"
+            error={lastNameValidation.error && resLastName.length > 0}
             variant="standard"
             onChange={(event) => { setLastName(event.target.value); }}
           />
           <TextField
             id="new-resident-email"
             label="Email"
+            error={emailValidation.error && resEmail.length > 0}
             variant="standard"
             onChange={(event) => { setEmail(event.target.value); }}
           />
           <TextField
             id="new-resident-phone"
             label="Phone Number"
+            error={phoneValidation.error && resPhone.length > 0}
             variant="standard"
             onChange={(event) => { setPhone(event.target.value); }}
           />
           <Stack direction="row" spacing={1}>
             <Button variant="contained" onClick={onClose}>Cancel</Button>
-            <Button variant="contained" onClick={submitResident}>Create</Button>
+            <Button
+              variant="contained"
+              disabled={
+                rankValidation.error
+                || firstNameValidation.error
+                || middleNameValidation.error
+                || lastNameValidation.error
+                || emailValidation.error
+                || phoneValidation.error
+              }
+              onClick={submitResident}
+            >
+              Create
+            </Button>
           </Stack>
         </Stack>
       </Box>
