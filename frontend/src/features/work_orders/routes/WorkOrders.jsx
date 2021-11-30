be8048d6dade0900e8693aafe5f8c2c6d6e02b23
Michael Pingleton <mnpingleton@gmail.com>
2021-11-30 11:22:22 -0600

    Selectors are laid out according to the type of filter selected.

--- frontend/src/features/work_orders/routes/WorkOrders.jsx
+++ frontend/src/features/work_orders/routes/WorkOrders.jsx
@@ -25,6 +25,35 @@ export const WorkOrders = () => {
   const [selectedBuildingId, setSelectedBuildingId] = React.useState(0);
   const [selectedRoomId, setSelectedRoomId] = React.useState(0);
 
+  let filterSelectors = null;
+  if (filterType === 'room') {
+    filterSelectors = (
+      <Stack direction="row" spacing={1}>
+        <BaseSelector
+          baseId={selectedBaseId}
+          onSelectionChanged={(baseId) => {
+            setSelectedBaseId(baseId);
+            setSelectedBuildingId(0);
+            setSelectedRoomId(0);
+          }}
+        />
+        <BuildingSelector
+          baseId={selectedBaseId}
+          buildingId={selectedBuildingId}
+          onSelectionChanged={(buildingId) => {
+            setSelectedBuildingId(buildingId);
+            setSelectedRoomId(0);
+          }}
+        />
+        <RoomSelector
+          buildingId={selectedBuildingId}
+          roomId={selectedRoomId}
+          onSelectionChanged={(roomId) => { setSelectedRoomId(roomId); }}
+        />
+      </Stack>
+    );
+  }
+
   return (
     <ContentLayout title="Work Orders">
       <NewWorkOrderDialog
@@ -62,27 +91,7 @@ export const WorkOrders = () => {
             <ToggleButton value="me">Created By Me</ToggleButton>
             <ToggleButton value="room">By Building/Room</ToggleButton>
           </ToggleButtonGroup>
-          <BaseSelector
-            baseId={selectedBaseId}
-            onSelectionChanged={(baseId) => {
-              setSelectedBaseId(baseId);
-              setSelectedBuildingId(0);
-              setSelectedRoomId(0);
-            }}
-          />
-          <BuildingSelector
-            baseId={selectedBaseId}
-            buildingId={selectedBuildingId}
-            onSelectionChanged={(buildingId) => {
-              setSelectedBuildingId(buildingId);
-              setSelectedRoomId(0);
-            }}
-          />
-          <RoomSelector
-            buildingId={selectedBuildingId}
-            roomId={selectedRoomId}
-            onSelectionChanged={(roomId) => { setSelectedRoomId(roomId); }}
-          />
+          {filterSelectors}
         </Stack>
         <WorkOrderList
           listType={filterType}
