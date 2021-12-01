49d2703573685b6032818cf96f45cd02ed59548e
Michael Pingleton <mnpingleton@gmail.com>
2021-12-01 11:16:02 -0600

    Added filter selectors.

--- frontend/src/features/inspections/routes/Inspections.jsx
+++ frontend/src/features/inspections/routes/Inspections.jsx
@@ -12,6 +12,10 @@ import { ContentLayout } from '@/components/layout';
 import { InspectionsList } from '../components/InspectionsList';
 import { NewInspectionsDialog } from '../components/NewInspectionsDialog';
 import { ViewInspectionDetailsDialog } from '../components/ViewInspectionDetailsDialog';
+import { BaseSelector } from '@/components/BaseSelector';
+import { BuildingSelector } from '@/components/BuildingSelector';
+import { RoomSelector } from '@/components/RoomSelector';
+import { PersonnelSelector } from '@/components/PersonnelSelector';
 
 export const Inspections = () => {
   const [currentInspectionSelection, setInspectionSelection] = React.useState([]);
@@ -20,6 +24,60 @@ export const Inspections = () => {
     setViewInspectionDetailsDialogOpen] = React.useState(false);
 
   const [filterType, setFilterType] = React.useState('all');
+  const [selectedBaseId, setSelectedBaseId] = React.useState(0);
+  const [selectedPersonnelId, setSelectedPersonnelId] = React.useState(0);
+  const [selectedBuildingId, setSelectedBuildingId] = React.useState(0);
+  const [selectedRoomId, setSelectedRoomId] = React.useState(0);
+
+  let filterSelectors = null;
+  if (filterType === 'inresident') {
+    filterSelectors = (
+      <Stack direction="row" spacing={1}>
+        <BaseSelector
+          baseId={selectedBaseId}
+          onSelectionChanged={(baseId) => {
+            setSelectedBaseId(baseId);
+            setSelectedPersonnelId(0);
+          }}
+        />
+        <PersonnelSelector
+          baseId={selectedBaseId}
+          personnelId={selectedPersonnelId}
+          onSelectionChanged={(personnelId) => {
+            setSelectedPersonnelId(personnelId);
+          }}
+        />
+      </Stack>
+    );
+  } else if (filterType === 'inroom') {
+    filterSelectors = (
+      <Stack direction="row" spacing={1}>
+        <BaseSelector
+          baseId={selectedBaseId}
+          onSelectionChanged={(baseId) => {
+            setSelectedBaseId(baseId);
+            setSelectedBuildingId(0);
+            setSelectedRoomId(0);
+          }}
+        />
+        <BuildingSelector
+          baseId={selectedBaseId}
+          buildingId={selectedBuildingId}
+          onSelectionChanged={(buildingId) => {
+            setSelectedBuildingId(buildingId);
+            setSelectedRoomId(0);
+          }}
+        />
+        <RoomSelector
+          buildingId={selectedBuildingId}
+          roomId={selectedRoomId}
+          onSelectionChanged={(roomId) => {
+            setSelectedRoomId(roomId);
+          }}
+        />
+      </Stack>
+    );
+  }
 
   return (
     <ContentLayout title="Inspections">
@@ -49,13 +107,16 @@ export const Inspections = () => {
           </Button>
           <ToggleButtonGroup
             value={filterType}
-            onChange={(event) => { setFilterType(event.target.value); }}
+            onChange={(event) => {
+              setFilterType(event.target.value);
+            }}
           >
             <ToggleButton value="inroom">For Selected Room</ToggleButton>
             <ToggleButton value="inresident">For Selected Resident</ToggleButton>
             <ToggleButton value="byme">Created By Me</ToggleButton>
             <ToggleButton value="all">All</ToggleButton>
           </ToggleButtonGroup>
+          {filterSelectors}
         </Stack>
         <InspectionsList
           listType={filterType}
