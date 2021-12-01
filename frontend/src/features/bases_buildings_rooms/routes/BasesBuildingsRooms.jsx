4e92853a00fbd4dd073efd4517fc41087f455026
Michael Pingleton <mnpingleton@gmail.com>
2021-12-01 17:11:56 -0600

    Added initial stack structure for bases buildings rooms page.

--- frontend/src/features/bases_buildings_rooms/routes/BasesBuildingsRooms.jsx
+++ frontend/src/features/bases_buildings_rooms/routes/BasesBuildingsRooms.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 
+import {
+  Stack,
+  Button,
+} from '@mui/material';
+
 import { ContentLayout } from '@/components/layout';
 
 export const BasesBuildingsRooms = () => (
   <ContentLayout title="Manage Bases, Buildings, and Rooms">
-    Hello world!
+    <Stack
+      direction="row"
+      justifyContent="space-evenly"
+      spacing={1}
+      sx={{ width: '100%', height: '100%' }}
+    >
+      <Stack direction="column" spacing={1} sx={{ width: '100%' }}>
+        <Stack direction="row" spacing={1}>
+        </Stack>
+        // Base list
+      </Stack>
+      <Stack direction="column" spacing={1} sx={{ width: '100%' }}>
+        <Stack direction="row" spacing={1}>
+        </Stack>
+        // Buildings list
+      </Stack>
+      <Stack direction="column" spacing={1} sx={{ width: '100%' }}>
+        <Stack direction="row" spacing={1}>
+        </Stack>
+        // Rooms list
+      </Stack>
+    </Stack>
   </ContentLayout>
 );
 
