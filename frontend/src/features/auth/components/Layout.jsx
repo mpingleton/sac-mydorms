9afee4620352894dac768972c8e1290781fafe61
Michael Pingleton <mnpingleton@gmail.com>
2021-12-05 20:29:49 -0600

    Removed striker logo.

--- frontend/src/features/auth/components/Layout.jsx
+++ frontend/src/features/auth/components/Layout.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Container, Box, Link, Typography, Card,
+  Container, Box, Typography, Card,
 } from '@mui/material';
 
 import { Head } from '@/components/elements/Head';
-import SACPatchImage from '@/assets/sac-patch.png';
 
 export const Layout = ({ children, title }) => (
   <>
@@ -15,12 +14,6 @@ export const Layout = ({ children, title }) => (
       sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh', sm: { px: 6 }, lg: { px: 8 } }}
     >
       <Box sx={{ mx: [0, 'auto'], width: ['auto', '100%'], maxWidth: ['none', '28rem'] }}>
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Link href="/" sx={{ display: 'flex', alignItems: 'center' }}>
-            <Box component="img" src={SACPatchImage} height={100} p={3} />
-          </Link>
-        </Box>
-
         <Typography
           variant="h2"
           sx={{ textAlign: 'center', fontWeight: 800, fontSize: '1.875rem', lineHeight: '2.25rem' }}
